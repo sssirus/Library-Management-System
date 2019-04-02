c7988243039d4698fa287234b06a520fd9fc1bf8
yuelingxi <742805630@qq.com>
2019-04-02 14:32:31 +0800

    change

--- src/main/java/com/qa/demo/algorithm/AlgorithmDriverImpl.java
+++
@@ -1,4 +0,0 @@
-package com.qa.demo.algorithm;
-
-public class AlgorithmDriverImpl {
-}
--- src/main/java/com/qa/demo/dataStructure/PredicateType.java
+++
@@ -1,5 +0,0 @@
-package com.qa.demo.dataStructure;
-
-public enum PredicateType {
-    OBJECTPROPERTY, DATATYPEPROPERTY;
-}
